3037751e3ce4437b4b157cebf7aa6ac1e9f10300
nucknine <nucknine@gmail.com>
2018-04-28 10:27:52 +0300

    homework new version

--- src/index.js
+++ src/index.js
@@ -74,14 +74,11 @@ function returnCounter(number=0) {
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
+
 function returnArgumentsArray() {
-    var arr = [];
+    var [...rest] = arguments;
 
-    for (var i = 0; i < arguments.length; i++) {
-        arr.push(arguments[i]);
-    }
-
-    return arr;
+    return rest;
 }
 
 /*
@@ -100,14 +97,10 @@ function returnArgumentsArray() {
    console.log(newSum()) выведет 6
  */
 function bindFunction(fn) {
-    var args = [];
-
-    for (var i = 1; i < arguments.length; i++) {
-        args.push(arguments[i]);
-    }
+    var [, ...rest] = arguments;
 
     return function() {
-        return fn.apply(this, args);
+        return fn.apply(this, rest);
     }
 }
 
